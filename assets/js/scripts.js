a2fe565c88485c17eb3cda164c6f792abad1fa9f
Tim Lee <awesometim1@gmail.com>
2018-05-09 10:40:21 -0400

    feat: New design

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,38 @@
+// Home page redirect
+function homepage(){
+	window.location.href = "/"
+}
 // Typing effect
 
 var i = 0;
-var txt = "Hi! I'm Tim, A Full Stack Web Developer";
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-document.getElementById("type").innerHTML = "";
+var txt = "Hi! I'm Tim and I create AWESOME websites.";
 
-function typeWriter() {
+var banner = document.getElementsByClassName('banner')[0];
+var typeTxt = banner.getElementsByTagName('*');
+
+function typer() {
   if (i < txt.length) {
-    document.getElementById("type").innerHTML += txt.charAt(i);
+    typeTxt[0].innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typer, 50);
   }
 }
-typeWriter();
+typer();
+
+
+// function typeWriter(ind, typeList, typeTxt) {
+// 	if (i < typeList[ind].length) {
+// 		typeTxt[ind].innerHTML += typeList[ind].charAt(i);
+// 		i++;
+// 		setTimeout(typeWriter(ind,typeList,typeTxt), 50);
+// 	}
+// }
+// typeWriter(0, typeList, typeTxt);
+// for (var ind = 0; ind < typeList.length; ind ++){
+
+// 	i = 0;
+// }
+
 
 // Navbar Toggle
 function dropdown() {
@@ -27,15 +47,48 @@ function dropdown() {
 		btn.className = "hamburger hamburger--spin"
 	}
 	// Navbar dropdown
-	if (mNav.className === "navbar") {
+	if (mNav.className.indexOf("dropdown") < 0) {
 		mNav.className += " dropdown";
 	} else {
-		mNav.className = "navbar";
+		mNav.className = mNav.className.substring(0, mNav.className.length - 9);
 	}
 	// Navbar links
-	if (links.className === "links") {
+	if (links.className.indexOf("dropdown") < 0) {
         links.className += " dropdown";
     } else {
-        links.className = "links";
+        links.className = links.className.substring(0, links.className.length - 9)
     }
+}
+
+// Navbar color change
+var navBar = document.getElementById('mainNav');
+var bodyTag = document.getElementsByClassName('banner')[0];
+
+window.onscroll = function () {
+    "use strict";
+    if ($(window).scrollTop() >= window.innerHeight * .15 /4) {
+        navBar.classList.add("nav-colored");
+        navBar.classList.remove("nav-transparent");
+    }
+    else {
+    	navBar.classList.remove("nav-colored");
+    	navBar.classList.add("nav-transparent");
+
+
+    }
+};
+
+(function (){
+	navBar.classList.add('nav-transparent');
+})();
+
+// Navbar transparency fix
+
+function transp() {
+	console.log($('nav').height(), window.innerHeight * .15);
+	if ($('nav').height() <= window.innerHeight * .15) {
+		return true;
+	}
+	return false;
+
 }
